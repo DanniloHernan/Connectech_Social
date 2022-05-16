e6a03ed7dc03fccd9c5872c66ee1fbe731437921
Your Name <dannilohernan@gmail.com>
2022-05-16 11:30:35 -0500

    first commit

---
+++ T_C_delete.sql
@@ -0,0 +1,7 @@
+DELETE FROM comentarios WHERE id_comentario = '92b2de70-08ed-11ec-b0f7-bf70ea09e583'
+
+DELETE FROM conocimientos WHERE id_conocimiento = ''
+
+DELETE FROM datos_usuarios WHERE id_datosUsuario = ''
+
+DELETE FROM usuarios WHERE id_Usuario = ''
